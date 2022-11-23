6636e4ba6badd9aaaf6a29be5f310f4b430830da
Christian Robles <8890471+roblesch@users.noreply.github.com>
2022-11-22 16:46:09 -0800

    Initial commit

---
+++ renderer/src/renderer.cpp
@@ -0,0 +1,12 @@
+﻿// renderer.cpp : Defines the entry point for the application.
+//
+
+#include "renderer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
