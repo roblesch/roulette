7331a15d08d019f8485f7c02056dafadbfb35a85
roblesch <8890471+roblesch@users.noreply.github.com>
2023-01-18 13:57:47 -0800

    cleanup comment

--- renderer/src/pathtracer.cpp
+++ renderer/src/pathtracer.cpp
@@ -33,15 +33,10 @@ Vec3f PathTracer::trace(const Scene& scene, const Vec2i& px) {
     int maxBounces = 64;
     Vec3f emission(0.0f, 0.0f, 0.0f);
     bool hit = scene.intersect(ray, intersection);
-    while (hit && bounce < maxBounces) {
-    }
 
     /*
-     commit 92c8882193b6baf30d8392e46db7502d18b81453 (HEAD)
-     Author: Benedikt Bitterli <mail@noobody.org>
-     Date:   Fri Apr 18 16:22:12 2014 +0200
+    92c8882193b6baf30d8392e46db7502d18b81453
 
-    Initial commit
     dir = cam.sampleDir
     intersect ray(dir)
     vec3f throughput(1.0f)
@@ -72,11 +67,7 @@ Vec3f PathTracer::trace(const Scene& scene, const Vec2i& px) {
         emission += throughput * estimateDirect(frame, material.bsdf, event, p, Ns, bsdfXi1, lightXi)
 
       material.bsdf->sample(event)
-
       throughput *= abs(event.wi.z) * event.throughput/event.pdf
-      
-
-
     */
 
     return Vec3f(0.0f);
